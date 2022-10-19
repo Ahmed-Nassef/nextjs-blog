9cd02b8ce133094c9c1796612cfc7fd6cff51604
vnz_ <mahmoudnasef2030@gmail.com>
2022-10-19 09:56:36 +0200

    fixing bug

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,20 @@
 import Head from "next/head";
-import Link from "next/link";
-import styles from "../styles/Home.module.css";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
-export default function Home() {
+import { getSortedPostsData } from "../lib/posts.js";
+
+export const getStaticProps = async () => {
+  const allPosts = getSortedPostsData();
+
+  return {
+    props: {
+      allPosts,
+    },
+  };
+};
+
+export default function Home({ allPosts }) {
+  console.log("all posts", allPosts);
   return (
     <Layout Home>
       <Head>
@@ -15,6 +26,20 @@ export default function Home() {
           <br /> i will be writing blogs here
         </p>
         <p></p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPosts.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
