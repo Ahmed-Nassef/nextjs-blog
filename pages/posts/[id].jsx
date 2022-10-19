eccadd5adee476d399c88fb7d0b3676290b08988
vnz_ <mahmoudnasef2030@gmail.com>
2022-10-19 11:43:07 +0200

    finished

--- pages/posts/[id].jsx
+++ pages/posts/[id].jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import Layout from "../../components/layout";
+import Date from "../../components/Date";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
 
-const Post = ({ postData }) => {
+// Add this import at the top of the file
+import utilStyles from "../../styles/utils.module.css";
+
+export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>{postData.Date}</div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
-};
+}
+
 export const getStaticPaths = async () => {
   // Return a list of possible value for id
 
@@ -24,11 +33,10 @@ export const getStaticPaths = async () => {
 };
 export const getStaticProps = async ({ params }) => {
   // Fetch necessary data for the blog post using params.id
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
     },
   };
 };
-export default Post;
